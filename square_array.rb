4859b00f0d723bf53f43f9aedd2b042e5f2d35ac
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 02:02:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   next_one = 0
   array.each do |each_one|
-    product each_one * each_one
+    product = each_one * each_one
     next_one += 1
   end
-  
+  product
 end
